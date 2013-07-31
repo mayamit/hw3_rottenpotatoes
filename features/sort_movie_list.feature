687283a1dc20aff5295a46f6535af5a68afaf553
Amit Gandhi <mayamit@yahoo.com>
2013-07-31 09:11:45 -0700

    Completing all the assignments for HW3

--- features/sort_movie_list.feature
+++ features/sort_movie_list.feature
@@ -24,8 +24,13 @@ Background: movies have been added to database
 Scenario: sort movies alphabetically
   When I follow "Movie Title"
   # your steps here
+  Then I should see "Aladdin" before "Amelie"
+  And I should see "Chicken Run" before "Chocolat"
+  And I should see "Chocolat" before "The Incredibles"
 
 Scenario: sort movies in increasing order of release date
   When I follow "Release Date"
-  # your steps here
+  Then I should see "Raiders of the Lost Ark" before "The Terminator"
+  And I should see "When Harry Met Sally" before "Aladdin"
+  And I should see "The Incredibles" before "The Help"
 
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -17,7 +17,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  assert(page.body.index(e1)<page.body.index(e2))
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -28,15 +28,6 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-#if uncheck.nil?
-#    rating_list.split(",").each do |r|
-#      step "I check \"ratings[#{r.strip}]\""
-#    end
-#  else
-#    rating_list.split(",").each do |r|
-#      step "I uncheck \"ratings[#{r.strip}]\""
-#    end
-#  end
   rating_list.split(",").each do |r|
     step "I #{uncheck}check \"ratings[#{r.strip}]\""
   end
