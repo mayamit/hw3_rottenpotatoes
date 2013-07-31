27c760f1cb1bac0da2caff5717630738f2e10bcd
Amit Gandhi <mayamit@yahoo.com>
2013-07-31 05:15:34 -0700

    Implementing HW3 Part 3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,9 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    if Movie.find(:all, :conditions => ['title = ?', movie['title']]).nil?
-      Movie.create!(movie)
-    end
+#  if Movie.find(:all, :conditions => ['title = ?', movie['title']]).nil?
+   Movie.create(movie)
+#    end
   end
 #flunk "Unimplemented"
 end
@@ -28,4 +28,25 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  if uncheck.nil?
+    rating_list.split(",").each do |r|
+      step "I check \"ratings[#{r.strip}]\""
+    end
+  else
+    rating_list.split(",").each do |r|
+      step "I uncheck \"ratings[#{r.strip}]\""
+    end
+  end
+end
+
+Then /^I should see all of the movies$/ do
+  count=Movie.count
+  actual_count=page.all('#movies tr').size-1
+  actual_count.should==count
+end
+
+Then /^I should not see all of the movies$/ do
+  count=0
+  actual_count=page.all('#movies tr').size-1
+  actual_count.should==count
 end
